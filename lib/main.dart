8b76f62177242b6d57910435e8e08e408c2f72e1
Rajeel Ansari <51192167+rajeeell@users.noreply.github.com>
2023-06-05 18:37:33 +0530

    added comments to ui_updates_demo.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,32 +8,32 @@ import 'package:flutter_internals/keys/keys.dart';
 
 // define the main function that runs the app
 void main() {
- // call the runApp function and pass an instance of the App widget as an argument
- runApp(const App());
+  // call the runApp function and pass an instance of the App widget as an argument
+  runApp(const App());
 }
 
 // define a stateless widget class named App
 class App extends StatelessWidget {
- // use a named parameter to initialize the key property of the superclass
- const App({super.key});
+  // use a named parameter to initialize the key property of the superclass
+  const App({super.key});
 
- // override the build method to return a widget tree
- @override
- Widget build(BuildContext context) {
-  // return a MaterialApp widget that provides some basic app configuration
-  return MaterialApp(
-   // set the theme property to use Material 3 design system
-   theme: ThemeData(useMaterial3: true),
-   // set the home property to a Scaffold widget that provides a basic app layout
-   home: Scaffold(
-    // set the appBar property to an AppBar widget that displays a title bar
-    appBar: AppBar(
-     // set the title property to a Text widget that displays some text
-     title: const Text('Flutter Internals'),
-    ),
-    // set the body property to a Keys widget that displays some content
-    body: const Keys(),
-   ),
-  );
- }
+  // override the build method to return a widget tree
+  @override
+  Widget build(BuildContext context) {
+    // return a MaterialApp widget that provides some basic app configuration
+    return MaterialApp(
+      // set the theme property to use Material 3 design system
+      theme: ThemeData(useMaterial3: true),
+      // set the home property to a Scaffold widget that provides a basic app layout
+      home: Scaffold(
+        // set the appBar property to an AppBar widget that displays a title bar
+        appBar: AppBar(
+          // set the title property to a Text widget that displays some text
+          title: const Text('To Do List App'),
+        ),
+        // set the body property to a Keys widget that displays some content
+        body: const Keys(),
+      ),
+    );
+  }
 }
--- lib/ui_updates_demo.dart
+++ lib/ui_updates_demo.dart
@@ -1,34 +1,52 @@
+// import a package that provides widgets and tools for building Flutter apps
 import 'package:flutter/material.dart';
+// import a custom package that defines a stateful widget class named DemoButtons
 import 'package:flutter_internals/demo_buttons.dart';
 
+// define a stateless widget class named UIUpdatesDemo
 class UIUpdatesDemo extends StatelessWidget {
-  const UIUpdatesDemo({super.key});
+ // use a named parameter to initialize the key property of the superclass
+ const UIUpdatesDemo({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    print('UIUpdatesDemo BUILD called');
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            Text(
-              'Every Flutter developer should have a basic understanding of Flutter\'s internals!',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Do you understand how Flutter updates UIs?',
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 24),
-            DemoButtons(),
-          ],
-        ),
+ // override the build method to return a widget tree
+ @override
+ Widget build(BuildContext context) {
+  // print a message to the console for debugging purposes
+  print('UIUpdatesDemo BUILD called');
+  // return a Padding widget that adds some space around its child
+  return Padding(
+   // set the padding property to an EdgeInsets object that defines the space in all directions
+   padding: const EdgeInsets.all(8.0),
+   // set the child property to a Center widget that centers its child
+   child: Center(
+    // set the child property to a Column widget that arranges its children vertically
+    child: Column(
+     // set the mainAxisSize property to min to shrink the column to fit its children
+     mainAxisSize: MainAxisSize.min,
+     // set the crossAxisAlignment property to center to align the children at the center of the column
+     crossAxisAlignment: CrossAxisAlignment.center,
+     // set the children property to a list of widgets
+     children: const [
+      // add a Text widget that displays some text with bold style
+      Text(
+       'Every Flutter developer should have a basic understanding of Flutter\'s internals!',
+       textAlign: TextAlign.center,
+       style: TextStyle(fontWeight: FontWeight.bold),
       ),
-    );
-  }
+      // add a SizedBox widget that adds some vertical space
+      SizedBox(height: 16),
+      // add another Text widget that displays some text
+      Text(
+       'Do you understand how Flutter updates UIs?',
+       textAlign: TextAlign.center,
+      ),
+      // add another SizedBox widget that adds some vertical space
+      SizedBox(height: 24),
+      // add a DemoButtons widget that displays some buttons and responds to taps
+      DemoButtons(),
+     ],
+    ),
+   ),
+  );
+ }
 }
